7cf0d7bcd82481a8c2ce0a4090573ff933769553
Mateus Junges <mateus@junges.dev>
2020-08-01 17:47:58 -0300

    Remove image resize

--- FrameExtraction/image_frame_extraction.py
+++ FrameExtraction/image_frame_extraction.py
@@ -19,12 +19,10 @@ def extract_video_frames(path_to_video, save_frames_to, framerate=None):
 
         if framerate is not None:
             if i % framerate == 0:
-                resized_frame = resize(frame, (48, 48))
-                img = Image.fromarray(cvtColor(resized_frame, COLOR_BGR2RGB))
+                img = Image.fromarray(cvtColor(frame, COLOR_BGR2RGB))
                 img.save(save_frames_to + '/' + str(i) + '.jpg')
         else:
-            resized_frame = resize(frame, (48, 48))
-            img = Image.fromarray(cvtColor(resized_frame, COLOR_BGR2RGB))
+            img = Image.fromarray(cvtColor(frame, COLOR_BGR2RGB))
             img.save(save_frames_to + '/' + str(i) + '.jpg')
         i += 1
     cap.release()
